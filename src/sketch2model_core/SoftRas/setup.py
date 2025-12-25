a370deec2fe79c025134e7d37ac1a029811c3ae5
danie1sung <daniel.suh.sung@gmail.com>
2025-12-26 01:44:29 +0900

    change-setup

--- src/sketch2model_core/SoftRas/setup.py
+++ src/sketch2model_core/SoftRas/setup.py
@@ -23,7 +23,7 @@ ext_modules = [
         ]),
     ]
 
-INSTALL_REQUIREMENTS = ['numpy', 'torch', 'torchvision', 'scikit-image', 'tqdm', 'imageio']
+## INSTALL_REQUIREMENTS = ['numpy', 'torch', 'torchvision', 'scikit-image', 'tqdm', 'imageio']
 
 setup(
     description='PyTorch implementation of "Soft Rasterizer"',
